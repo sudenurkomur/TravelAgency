bc4bae263d241c02e7571194b9df148c034e374a
Sude Nur Komur <sudenurkomur1253@gmail.com>
2024-04-23 00:56:20 +0300

    No Graph Anymore

---
+++ src/HashMap.java
@@ -0,0 +1,34 @@
+// Java program to remove
+// elements from HashMap
+
+import java.io.*;
+import java.util.*;
+class RemoveElementsOfHashMap{
+    public static void main(String args[])
+    {
+        // Initialization of a HashMap
+        Map<Integer, String> hm = new HashMap<Integer, String>();
+
+        // Add elements using put method
+        hm.put(1, "Geeks");
+        hm.put(2, "For");
+        hm.put(3, "Geeks");
+        hm.put(4, "For");
+
+        // Initial HashMap
+        System.out.println("Mappings of HashMap are : "
+                + hm);
+
+        // remove element with a key
+        // using remove method
+        hm.remove(3);
+
+        // Final HashMap
+        System.out.println("Mappings after removal are : "
+                + hm);
+
+        for (Map.Entry<Integer, String> e : hm.entrySet())
+            System.out.println("Key: " + e.getKey()
+                    + " Value: " + e.getValue());
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,78 @@
-public class Main {
+// Java Program to Create
+// HashMap in Java
+import java.util.HashMap;
 
-    public static void main(String args[]) {
-        
+// Driver Class
+public class Main {
+    // main function
+    public static void main(String[] args) {
+
+        // İzmir'in bilgilerini içeren bir obje oluşturalım
+        LandMarkInformations izmir = new LandMarkInformations("Hotel", 25.0 ,4);
+        LandMarkInformations sude = new LandMarkInformations("uzay", 24.0 ,4);
+
+
+        // Tek bir anahtar altında iki değeri saklayacak bir HashMap oluşturalım
+        HashMap<String, LandMarkInformations> izmirBilgileri = new HashMap<>();
+        izmirBilgileri.put("Izmir", izmir);
+        izmirBilgileri.put("Sude", sude);
+
+        // Anahtarı kullanarak İzmir'in bilgilerine erişelim
+        LandMarkInformations izmirBilgi = izmirBilgileri.get("Izmir");
+        System.out.println("İzmir'in yüzölçümü: " + izmirBilgi.getName() );
+        System.out.println("İzmir'in sıcaklığı: " + izmirBilgi.getLoad() );
+
+        LandMarkInformations sudeBilgi = izmirBilgileri.get("Sude");
+        System.out.println("İzmir'in yüzölçümü: " + sudeBilgi.getName() );
+        System.out.println("İzmir'in sıcaklığı: " + sudeBilgi.getLoad() );
+        System.out.println("İzmir'in sıcaklığı: " + sudeBilgi.getInterest() );
+    }
+}
+
+ class LandMarkInformations {
+    private String name;
+    private double load;
+    private double interest;
+
+    public LandMarkInformations(String name, double load ,double interest) {
+        this.name = name;
+        this.load = load;
+        this.interest=interest;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getLoad() {
+        return load;
+    }
+
+     public double getInterest() {
+         return interest;
+     }
+}
+
+class OtherLandMarkInformations {
+    private String name;
+    private double destination;
+    private double attractiveness;
+
+    public OtherLandMarkInformations(String name, double destination,double attractiveness) {
+        this.name = name;
+        this.destination = destination;
+        this.attractiveness=attractiveness;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getDestination() {
+        return destination;
+    }
+
+    public double getAttractiveness() {
+        return attractiveness;
     }
 }
