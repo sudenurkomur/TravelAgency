5ff759af34e7c8815acf9beb3ce74fd0de8ed7e4
Sude Nur Komur <sudenurkomur1253@gmail.com>
2024-04-26 00:47:04 +0300

    trying with arraylist

--- src/Main.java
+++ src/Main.java
@@ -1,69 +1,82 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 
-public class Main {
-    public static void main(String[] args) {
-
-        String[] filePaths = {"/Users/sudenurkomur/IdeaProjects/TravelAgency/src/landmark_map_data.txt",
-                //"/Users/sudenurkomur/IdeaProjects/TravelAgency/src/personal_interest.txt",
-                //"/Users/sudenurkomur/IdeaProjects/TravelAgency/src/visitor_load.txt"
-        };
-
-        for (String filePath : filePaths) {
-            ProcessFile.process(filePath);
-        }
+class Record
+{
+    public String source;
+    public String dest;
+    public Double score;
+    public Double time;
+    public Record(String source, String dest, Double score, Double time){
+        this.source = source;
+        this.dest = dest;
+        this.score = score;
+        this.time = time;
     }
 
-    // Inner class for processing files
-    static class ProcessFile {
-        public static void process(String filePath) {
-            int lineCount;
-            BufferedReader objReader = null;
-            try {
-                String strCurrentLine;
+    public static void main(String[] args){
+        ArrayList<Record> arrayList = new ArrayList<>();
+        readLandMark(arrayList);
+        readInterestAndLoadAndModifyTheList(arrayList);
 
-                objReader = new BufferedReader(new FileReader(filePath));
-                // Finding the number of lines in the file
-                lineCount = 0;
-                while (objReader.readLine() != null) {
-                    lineCount++;
-                }
+        System.out.println();
 
-                // Array to store first spaces
-                Object[][] landmarkData = new Object[lineCount][7]; // Create an array of appropriate size
-
-                // Reading the content of the file line by line and splitting by spaces
-                String line;
-                int lineIncrease = 0;
-                while ((line= objReader.readLine())!= null && lineIncrease != lineCount) {
-                    String[] words = line.split("\\s+"); // Split by spaces
-                    System.out.println(words[1]);
-                    if (words.length >= 2) {
-                        landmarkData[lineIncrease][0] = words[0];
-                        landmarkData[lineIncrease][1] = words[1];
-                    }
-                    lineIncrease++;
-                }
-
-                /*for (int i = 0; i < lineCount; i++) {
-                    for(int m=0;m< 7;m++)
-                    {
-                        System.out.print(landmarkData[i][m] + " ");
-                    }
-                    System.out.println();
-                }*/
-
-                // Closing the used resources
-                objReader.close();
-
-
-            } catch (FileNotFoundException e) {
-                throw new RuntimeException(e);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+    }
+    public static void readLandMark(ArrayList<Record> arrayList){
+        try{
+            FileInputStream fstream = new FileInputStream("/Users/sudenurkomur/IdeaProjects/TravelAgency/src/landmark_map_data.txt");
+            DataInputStream in = new DataInputStream(fstream);
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String strLine;
+            strLine = br.readLine();
+            while ((strLine = br.readLine()) != null)   {
+                String[] tokens = strLine.split("\t");
+                Record record = new Record(tokens[0].trim(),tokens[1].trim(),Double.parseDouble(tokens[2].trim()),Double.parseDouble(tokens[3].trim()));//process record , etc
+                arrayList.add(record);
             }
+            in.close();
+            System.out.println();
+        } catch (Exception e){
+            System.err.println("Error: " + e.getMessage());
         }
+        System.out.println();
     }
+
+    public static void readInterestAndLoadAndModifyTheList(ArrayList<Record> arrayList){
+        try{
+            FileInputStream fstream = new FileInputStream("/Users/sudenurkomur/IdeaProjects/TravelAgency/src/visitor_load.txt");
+            DataInputStream in = new DataInputStream(fstream);
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String strLine;
+            HashMap<String,Double> Load = new HashMap<>();
+            strLine = br.readLine();
+            while ((strLine = br.readLine()) != null)   {
+                String[] tokens = strLine.split("\t");
+                String dest = tokens[0].trim();
+                Double load = (1- Double.parseDouble(tokens[1].trim()));
+                Load.put(dest,load);
+            }
+            fstream = new FileInputStream("/Users/sudenurkomur/IdeaProjects/TravelAgency/src/personal_interest.txt");
+            in = new DataInputStream(fstream);
+            br = new BufferedReader(new InputStreamReader(in));
+            strLine = br.readLine();
+            while ((strLine = br.readLine()) != null)   {
+                String[] tokens = strLine.split("\t");
+                String dest = tokens[0].trim();
+                Double interest = Double.parseDouble(tokens[1].trim());
+                Load.put(dest, Load.get(dest)*interest);
+            }
+            in.close();
+            System.out.println();
+            for(int i =0; i<arrayList.size(); i++){
+                Double temp = Load.get(arrayList.get(i).dest);
+                arrayList.get(i).score *= temp;
+            }
+        } catch (Exception e){
+            System.err.println("Error: " + e.getMessage());
+        }
+        System.out.println();
+    }
+
 }
